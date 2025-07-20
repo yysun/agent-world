279d040e5097bb83f5eed562addecdc2702b9248
Yiyi Sun <yiyisun@gmail.com>
2025-07-20 13:27:12 -0400

    feat: update development scripts and add Vite configuration for API proxy

---
+++ web/vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:3000',
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  }
+})
